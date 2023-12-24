974c0c25e0b7fda219366ebf77109c48e416da5a
Hao-Ting Wang <htwangtw@gmail.com>
2023-12-23 20:09:10 -0500

    compile cv

--- compile_cv.sh
+++ compile_cv.sh
@@ -6,10 +6,10 @@ pdflatex cv
 bibtex cv
 pdflatex cv
 
-git add .
-git commit -m "compile cv"
 cp cv.pdf ~/
 rm cv.pdf
+git add .
+git commit -m "compile cv"
 git checkout gh-pages
 mv ~/cv.pdf .
 git add cv.pdf
